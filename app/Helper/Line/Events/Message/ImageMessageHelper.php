8ef8b413a7b05340b4feb7fb62bb1f7a191b0648
austaer <austaer@uitox.com>
2019-09-03 18:45:12 +0800

    add image echo message bot

--- app/Helper/Line/Events/Message/ImageMessageHelper.php
+++ app/Helper/Line/Events/Message/ImageMessageHelper.php
@@ -4,12 +4,20 @@
 namespace App\Helper\Line\Events\Message;
 
 
+use Illuminate\Support\Carbon;
+use Illuminate\Support\Str;
+
 class ImageMessageHelper extends AbstractMessageHelper
 {
 
     public function reply()
     {
-        // TODO: Implement reply() method.
-        $this->log('info', "Enter TextMessage");
+        $messageId = $this->event->getMessageId();
+        $response = $this->bot->getMessageContent($messageId);
+        if ($response->isSucceeded()) {
+            $fileName = Carbon::now()->format('YmdHis') . Str::random(10);
+            \Storage::disk('public')->put("img/{$fileName}", $response->getRawBody());
+            $this->bot->replyText($this->replyToken, "收到圖片啦. 連結: " . \Storage::disk('public')->url("img/{$fileName}"));
+        }
     }
 }
